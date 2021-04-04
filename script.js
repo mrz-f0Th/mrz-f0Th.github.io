a4fc8231a82a5bb48fd3bcd4a9d9711beabf022b
mrz-f0Th <rakazzakari22@gmail.com>
2021-04-04 07:32:15 -0700

    menambahkan projek real & mempercantik tampilan

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const zoom = document.querySelector('.zoom');
 
 const destruct = [...zoom.textContent].map(h => `<span class = "destruct">${h}</span>`).join('');
-zoom.innerHTML = destruct
-console.log(destruct)
+zoom.innerHTML = destruct;
+
+const btnKirim = document.querySelector('.btn-kirim');
+btnKirim.addEventListener('click', () => {
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Maaf Form belum bisa di pakai',
+        width: 400,
+        // footer: '<a href>Why do I have this issue?</a>'
+    })
+})
